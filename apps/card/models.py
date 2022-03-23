5ad0d55ebf8407c2a609068598b7607c3b5a8e84
Ml06py <liaghimohamad69@gmail.com>
2022-03-23 14:16:49 +0430

    a model added

--- apps/card/models.py
+++ apps/card/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from account.models import User
 # Create your models here.
+class Transaction(models.Model):
+    class Choices(models.TextChoices):
+        add = "+","add"
+        cost = "-","cost"
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    action = models.CharField(max_length=1,choices=Choices.choices)
+    mount = models.PositiveSmallIntegerField(default=0,blank=True,null=True)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
