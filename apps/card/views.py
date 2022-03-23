a1814d7d9c22684171703e41333ca862911969fe
Ml06py <liaghimohamad69@gmail.com>
2022-03-24 01:56:00 +0430

    bug fixed

--- apps/card/views.py
+++ apps/card/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView,FormView
+from django.contrib import messages
 from account.models import User
 from .forms import CardForm
 from account.forms import RegisterForm
@@ -15,8 +16,9 @@ class Update(LoginRequiredMixin,FormView):
     def form_valid(self, form):
         forms = form.save(commit=False)
         forms.owner = self.request.user
-        User.objects.filter(username=self.request.user.username).update(fund=self.request.user.fund+forms.mount if forms.action=="+" else self.request.user.fund-forms.mount )
+        User.objects.filter(username=self.request.user.username).update(fund=self.request.user.fund+forms.mount if forms.action=="+" else exec('try:self.request.user.fund-forms.mount\nexcept:messages.success(self.request,("you dont have enough money!"))'))
         forms.save()
+        return  redirect("card:home")
     def form_invalid(self, form):
         print(form.errors)
 
