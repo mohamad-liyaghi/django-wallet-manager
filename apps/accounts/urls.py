5c5f930c3b414c23061ca5cae1964b67fec6ce01
Ml06py <liaghimohamad69@gmail.com>
2022-08-15 14:00:56 +0430

    Allauth logout added and extra files deleted

--- apps/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import  path
-from .views import Login,Register,Logout
-app_name = "accounts"
-urlpatterns = [
-    path("login/",Login.as_view(),name="login"),
-    path("register/",Register.as_view(),name="register"),
-    path("logout/",Logout,name="logout")
-]
