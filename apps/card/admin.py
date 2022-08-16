f10034f704d243ac1dbefb31280328d6750d9cbd
Ml06py <liaghimohamad69@gmail.com>
2022-08-16 13:14:52 +0430

    Transaction model updated and TransactionAdmin added

--- apps/card/admin.py
+++ apps/card/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Transaction
-# Register your models here.
-admin.site.register(Transaction)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("owner", "title", "amount", "action")
+    readonly_fields = ("title", "description", "action", "amount", "owner", "token")
--- apps/card/models.py
+++ apps/card/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from accounts.models import User
 
-# Create your models here.
+
 class Transaction(models.Model):
-    class Choices(models.TextChoices):
-        add = "+","add"
-        cost = "-","cost"
+
+    class Action(models.TextChoices):
+        add = ("+", "add")
+        cost = ("-", "cost")
+
     title = models.CharField(max_length=120)
     description = models.TextField()
-    action = models.CharField(max_length=1,choices=Choices.choices)
-    mount = models.PositiveSmallIntegerField(default=0)
-    owner = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
+    action = models.CharField(max_length=1, choices=Action.choices, default=Action.add)
+    amount = models.PositiveSmallIntegerField(default=0)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     token = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
